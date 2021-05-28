4d0d10a0b0adec5a465a57ffb70d2fcaacaf1396
Rivaan Ranawat <rivaanranawat@Rivaans-MacBook-Air.local>
2021-05-29 03:06:00 +0530

    redirecting to login screen if not logged in

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useContext } from "react";
+import { useHistory, Link } from "react-router-dom";
+import UserContext from "../context/UserContext";
 
 function Home() {
-    return (
-        <div>
-            From Home Screen
-        </div>
-    )
+  const { userData } = useContext(UserContext);
+  const history = useHistory();
+  useEffect(() => {
+    if (!userData.user) {
+      history.push("/login");
+    }
+  }, []);
+  return (
+    <div>
+      {userData.user ? (
+        <h1>Welcome {userData.user.username}</h1>
+      ) : (
+        <>
+          <h2>You are not logged in</h2>
+          <Link to="/login">Login</Link>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;

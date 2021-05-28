662b7544648692b6504abc5a2b5a392aa442e7b6
Rivaan Ranawat <rivaanranawat@Rivaans-MacBook-Air.local>
2021-05-29 02:45:40 +0530

    finishing signup from client side

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,11 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useContext } from "react";
 import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+import { useHistory } from "react-router";
+import UserContext from "../context/UserContext";
 
 function Signup() {
-  var loading = false;
-  var error = "";
-  function handleSubmit(e) {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [username, setUsername] = useState();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { setUserData } = useContext(UserContext);
+
+  const history = useHistory();
+
+  async function handleSubmit(e) {
     e.preventDefault();
+    setLoading(true);
+    try {
+      const newUser = { email, password, confirmPassword, username };
+      await axios.post("http://localhost:3001/signup", newUser);
+      const loginRes = await axios.post("http://localhost:3001/login", {
+        email,
+        password,
+      });
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+      setLoading(false);
+      history.push("/");
+    } catch (err) {
+      setLoading(false);
+      err.response.data.msg && setError(err.response.data.msg);
+    }
   }
   return (
     <Container
@@ -21,19 +52,35 @@ function Signup() {
               <Form onSubmit={handleSubmit}>
                 <Form.Group id="username">
                   <Form.Label>Username</Form.Label>
-                  <Form.Control type="name" required />
+                  <Form.Control
+                    type="name"
+                    required
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
                 </Form.Group>
                 <Form.Group id="email">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" required />
+                  <Form.Control
+                    type="email"
+                    required
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </Form.Group>
                 <Form.Group id="password">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" required />
+                  <Form.Control
+                    type="password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
                 </Form.Group>
                 <Form.Group id="password-confirm">
                   <Form.Label>Password Confirmation</Form.Label>
-                  <Form.Control type="password" required />
+                  <Form.Control
+                    type="password"
+                    required
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                  />
                 </Form.Group>
                 <Button disabled={loading} className="w-100 mt-2" type="submit">
                   Sign Up

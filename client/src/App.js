a4afd340232637a6a10f57b71b89b7863d797418
Rivaan Ranawat <rivaanranawat@Rivaans-MacBook-Air.local>
2021-05-29 02:30:03 +0530

    managing routes and isLogin on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,51 @@
 import Login from "./components/Login";
-import "./App.css";
 import Signup from "./components/Signup";
+import Home from "./components/Home";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import UserContext from "./context/UserContext";
 
 function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const isLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await axios.post(
+        "http://localhost:3001/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if (tokenRes.data) {
+        const userRes = await axios.get("http://localhost:3001/", {
+          headers: { "x-auth-token": token },
+        });
+        setUserData({
+          token,
+          user: userRes.data,
+        });
+      }
+    };
+    isLoggedIn();
+  }, []);
   return (
-        <Signup />
+    <BrowserRouter>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            From Home Screen
+        </div>
+    )
+}
+
+export default Home
